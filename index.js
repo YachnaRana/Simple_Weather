d425e03bc8a5e3ade78d956390c40495cc7bd208
YachnaRana <57913984+YachnaRana@users.noreply.github.com>
2020-07-24 19:04:14 -0400

    container using flex box

--- index.js
+++ index.js
@@ -24,44 +24,44 @@ async function getWeatherAW(woeid){
         const result = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}`);
         const data = await result.json();
         const today = data.consolidated_weather[0];
-        // console.log(today)
+        console.log(today)
 
         const predictability = document.querySelector('.predictability_value');
-        predictability.innerHTML = today.predictability;
-        // console.log("set predictability")
+        predictability.innerHTML = `${today.predictability}%`;
+        console.log("set predictability")
 
         const wind = document.querySelector('.wind_value');
-        wind.innerHTML = bound(today.wind_speed);
-        // console.log("set wind")
+        wind.innerHTML = `${bound(today.wind_speed)}mph`;
+        console.log("set wind")
 
         const humidity = document.querySelector('.humidity_value');
-        humidity.innerHTML = bound(today.humidity);
-        // console.log("set humidity")
+        humidity.innerHTML = `${bound(today.humidity)}%`;
+        console.log("set humidity")
 
         const temperature = document.querySelector('.temperature');
         temperature.innerHTML =bound(today.max_temp);
-        // console.log("set temp")
+        console.log("set temp")
 
         const max= document.querySelector('.max_temp');
         max.innerHTML = `${bound(today.max_temp)}`;
-        // console.log("set max temp")
+        console.log("set max temp")
 
         const min = document.querySelector('.min_temp');
         min.innerHTML = bound(today.min_temp);
-        // console.log("set min temp")
+        console.log("set min temp")
 
         const city = document.querySelector('.city');
         city.innerHTML = data.title;
-        // console.log("set city name")
+        console.log("set city name")
 
         const state = document.querySelector('.state');
         state.innerHTML = today.weather_state_name;
-        // console.log("set state")
+        console.log("set state")
 
         const weather_state_abbr = today.weather_state_abbr;
         weatherStateSVGChanger(weather_state_abbr);
 
-        return data;
+        // return data;
     } catch(error){
         alert(error);
     }
@@ -70,21 +70,14 @@ async function getWeatherAW(woeid){
 const bound = t => parseInt(t);
 const sliceDate = d => d.slice(6);
 const clearInput = () => {
-    document.querySelector('.search_field').value = '';
+    document.querySelector('#search_field').value = '';
+};
+const clearSvg = () => {
+    document.querySelector('.state_icon_svg').innerHTML = '';
 };
 
 async function Wrapper() {
-    let stringForSearch = await document.getElementById('search_field').value;
-    console.log(stringForSearch);
-    console.log(stringForSearch);
-    console.log(stringForSearch);
-    console.log(stringForSearch);
-    console.log(stringForSearch);
-    console.log(stringForSearch);
-    console.log(stringForSearch);
-    console.log(stringForSearch);
-    console.log(stringForSearch);
-    console.log(stringForSearch);
+    let stringForSearch = document.getElementById('search_field').value;
     let woeid = await getWeatherID(stringForSearch);
     await getWeatherAW(woeid);
 }
@@ -95,9 +88,10 @@ getWeatherAW(2459115);
 clearInput();
 
 document.getElementById('search_btn').addEventListener('click', e => {
-    Wrapper();
     e.preventDefault();
+    Wrapper();
     clearInput();
+    clearSvg();
 });
 
     
